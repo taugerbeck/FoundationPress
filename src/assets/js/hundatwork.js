10f8c34c7d57ad9c439415bfbfb69ebdf47fa622
taugerbeck <34959661+taugerbeck@users.noreply.github.com>
2017-12-30 20:15:19 +0100

    Add hundatwork.js

---
+++ src/assets/js/hundatwork.js
@@ -0,0 +1,24 @@
+function UnCryptMailto(s) {
+    var n = 0;
+    var r = "";
+    for ( var i = 0; i < s.length; i++) {
+        n = s.charCodeAt(i);
+        if (n >= 8364) {
+            n = 128;
+        }
+        r += String.fromCharCode(n - 1);
+    }
+    return r;
+}
+
+function linkTo_UnCryptMailto(s) {
+    location.href = UnCryptMailto(s);
+}
+
+window.___gcfg = {lang: 'de'};
+
+(function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
